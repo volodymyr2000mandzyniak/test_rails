6317ba7ce2e435bb89a76e23ae724397293e20e1
volodymyr2000mandzyniak <volodimirmandzyniak@gmail.com>
2022-12-30 23:40:15 +0200

    all CRUD

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,55 @@
+class ItemsController < ApplicationController
+  layout false 
+  skip_before_action :verify_authenticity_token
+  
+  def index
+    @items = Item.all    
+  end
+
+  def create
+    item = Item.create(items_params)
+    if item.persisted?  
+      redirect_to items_path
+    else
+      render json: item.errors, statud: :unprocessable_entity
+    end
+  end
+
+  def new; end
+
+  def show 
+    unless(@item = Item.where(id: params[:id]).first)
+      render body: 'Page not found', status: 404
+    end
+  end
+
+  def edit 
+    unless(@item = Item.where(id: params[:id]).first)
+      render body: 'Page not found', status: 404
+    end
+  end
+
+  def update
+    item = Item.where(id: params[:id]).first
+    if  item.update(items_params) 
+      redirect_to item_path
+    else
+      render json: items.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy 
+    item = Item.where(id: params[:id]).first.destroy
+    if item.destroyed?
+      redirect_to items_path
+    else
+      render json: item.errors, statud: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def items_params
+    params.permit(:price, :name, :real, :weight, :description)
+  end
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
     validates :price, numericality: {greater_than: 0}
-    validates :name, description:, presence: true
+    validates :name, presence: true
 end
+ 
